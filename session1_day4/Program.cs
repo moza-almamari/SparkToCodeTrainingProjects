2f20fd01d6aa33872ad13c9e464b9f872bdae9f8
Moza <mozaalmamri16@gmail.com>
2026-06-30 20:25:32 +0400

    Taking User Input

---
+++ session1_day4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace session1_day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Taking User Input
+            Console.Write("Please Enter Your Name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello " + userName);
+        }
+    }
+}
