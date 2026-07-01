8215119cdacfbce017ba9976cc2c369b5ab67e4a
Moza <mozaalmamri16@gmail.com>
2026-07-01 09:34:59 +0400

    Task 3 Even or Odd Checker

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,32 +4,43 @@
     {
         static void Main(string[] args)
         {
-            //1- Personal Info Card
-            string name = "Sara";
-            int age = 21;
-            double height = 1.65;
-            bool isStudent = true;
-            Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height +
-                ", Student: " + isStudent);
+            ////1- Personal Info Card
+            //string name = "Sara";
+            //int age = 21;
+            //double height = 1.65;
+            //bool isStudent = true;
+            //Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height +
+            //    ", Student: " + isStudent);
+
+            //////////////////////////////////////////////////////////////////
+
+            ////2- Rectangle Calculator
+            //Console.Write("Please enter the length of the rectangle: ");
+            //double length = double.Parse(Console.ReadLine());
+
+            //Console.Write("Please enter the width of the rectangle: ");
+            //double width = double.Parse(Console.ReadLine());
+
+            //double recArea = length * width;
+            //double recPerimeter = 2 * (length + width);
+
+            //Console.WriteLine("Rectangle area: " + recArea
+            //    + ", Rectangle perimeter: " + recPerimeter);
 
             ////////////////////////////////////////////////////////////////
 
-            //2- Rectangle Calculator
-            Console.Write("Please enter the length of the rectangle: ");
-            double length = double.Parse(Console.ReadLine());
+            //3- Even or Odd Checker
+            Console.Write("Please Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
 
-            Console.Write("Please enter the width of the rectangle: ");
-            double width = double.Parse(Console.ReadLine());
-
-            double recArea = length * width;
-            double recPerimeter = 2 * (length + width);
-
-            Console.WriteLine("Rectangle area: " + recArea
-                + ", Rectangle perimeter: " + recPerimeter);
-
-
-
-
-        }
+            if (number % 2 == 0 )
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
     }
+}
 }
