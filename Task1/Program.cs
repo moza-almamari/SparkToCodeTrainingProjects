dadbbe6a5e42704d39ea76fa24715ae1f172e60b
Moza <mozaalmamri16@gmail.com>
2026-07-01 08:56:32 +0400

    Task 1 Personal Info Card

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1- Personal Info Card
+            string name = "Sara";
+            int age = 21;
+            double height = 1.65;
+            bool isStudent = true;
+            Console.WriteLine("Name: " + name + ", Age: " + age + ", Height: " + height +
+                ", Student: " + isStudent);
+        }
+    }
+}
