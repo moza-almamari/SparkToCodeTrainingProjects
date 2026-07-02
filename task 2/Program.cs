c127df8f630288551afc3ce39176acafca3e3590
Moza <mozaalmamri16@gmail.com>
2026-07-02 10:17:24 +0400

    Task 2 - Sum of Numbers 1 to N

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
             //1- Countdown Timer
             Console.Write("Enter a starting number for the countdown: ");
             int countdownStart = int.Parse(Console.ReadLine());
@@ -13,6 +13,16 @@
                 Console.WriteLine(i);
             }
             Console.WriteLine("Liftoff!");
+            
+            //2- Sum of Numbers 1 to N
+            Console.Write("Enter a number: ");
+            int N = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum of numbers from 1 to {N} is: {sum}");
         }
 }
 }
