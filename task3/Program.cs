c5584c5de1c16ddf1e50d2520857e6a1fa91ff42
Moza <mozaalmamri16@gmail.com>
2026-07-05 21:46:43 +0400

    Task 1 - Absolute Difference

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1- Absolute Difference
+            Console.Write("Enter the first number: ");
+            int numberOne = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            int diff = Math.Abs(numberOne - numberTwo);
+            Console.WriteLine("Difference = " + diff);
+        }
+    }
+}
