3955b14d28d8f927d68baed893770380af6efbd0
Moza <mozaalmamri16@gmail.com>
2026-07-07 20:35:41 +0400

    Task 1 - Fixed Grades Array

--- task5/Program.cs
+++ task5/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1- Fixed Grades Array
+            int[] grades = new int[5];
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.Write("Enter grade " + (i + 1) + ": ");
+                grades[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\nStudent Grades:");
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
         }
     }
 }
