7b431c1c8de3542663a44a4731cc0e40c8eb90b8
Moza <mozaalmamri16@gmail.com>
2026-07-07 16:48:40 +0400

    Task 2 - Square Number Function

--- task 4/Program.cs
+++ task 4/Program.cs
@@ -7,14 +7,27 @@
         {
             Console.WriteLine("Welcome, " + name + "!");
         }
-
+        //2- Square Number Function
+        static int Square(int number)
+        {
+            return number * number;
+        }
         static void Main()
         {
+            /*
             // Prompt for the task 1  
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
 
             PrintWelcome(name);
+            */
+            // Prompt for the task 2
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int result = Square(number);
+
+            Console.WriteLine("Square = " + result);
         }
     }
 }
