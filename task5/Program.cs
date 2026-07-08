574eab4c0010feefcbb8278c6519e97d99b0446e
Moza <mozaalmamri16@gmail.com>
2026-07-08 10:14:37 +0400

    Task 2 - Dynamic To-Do List

--- task5/Program.cs
+++ task5/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             //1- Fixed Grades Array
             int[] grades = new int[5];
             for (int i = 0; i < grades.Length; i++)
@@ -16,7 +16,31 @@
             foreach (int grade in grades)
             {
                 Console.WriteLine(grade);
+            }*/
+            //2- Dynamic To-Do List
+            List<string> todoList = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter task " + (i + 1) + ": ");
+                string task = Console.ReadLine();
+                todoList.Add(task);
             }
+
+            Console.WriteLine("To-Do List:");
+            int number = 1;
+            foreach (string task in todoList)
+            {
+                Console.WriteLine(number + ". " + task);
+                number++;
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
