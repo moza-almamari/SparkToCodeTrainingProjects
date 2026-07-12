e97430f1edd69bbf5cfaa98434b142310a5dcda7
Moza <mozaalmamri16@gmail.com>
2026-07-12 20:40:00 +0400

    create class and first obj

---
+++ oop/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace oop
+{
+    class Student
+    {
+        public int Grade { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+     
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade1 = 60;
+            string name1 = "John";
+            string address1 = "123 Main St";
+
+
+            Student student1 = new Student(); // create obj
+        }
+    }
+}
