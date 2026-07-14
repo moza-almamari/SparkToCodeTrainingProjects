99a9eaec49dd826dac317e25449f98105bbfae71
Moza <mozaalmamri16@gmail.com>
2026-07-14 21:34:42 +0400

    add accunts

--- oop_s2/Program.cs
+++ oop_s2/Program.cs
@@ -19,7 +19,14 @@
     {
         static void Main(string[] args)
         {
-            
+            List<BankAccount> accounts = new List<BankAccount>();
+            BankAccount b1 = new BankAccount(1, 100);
+            accounts.Add(b1);
+            BankAccount b2 = new BankAccount(2, 205);
+            accounts.Add(b2);
+            accounts.Add(new BankAccount(3, 210));
+            accounts.Add(new BankAccount(4, 113));
+
         }
     }
 }
