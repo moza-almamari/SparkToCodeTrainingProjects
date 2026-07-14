5b0d0e7e502dddeea8d591f697eec999531cf01f
Moza <mozaalmamri16@gmail.com>
2026-07-14 21:33:45 +0400

    create BankAccount class

---
+++ oop_s2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace oop_s2
+{
+    public class BankAccount
+    {
+        public int AccountId { get; }
+        public string holderName;
+        public double balance;
+        public BankAccount(int Id, double amount)
+        {
+            AccountId = Id;
+            balance = amount;
+        }
+        public void checkBalance()
+        {
+            Console.WriteLine(balance);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
