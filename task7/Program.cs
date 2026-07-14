d7f706fd638a055b68ab0f87342404d6e68b538b
Moza <mozaalmamri16@gmail.com>
2026-07-14 21:10:14 +0400

    Room class

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,5 +1,22 @@
 ﻿namespace task7
 {
+    public class Room
+    {
+        public int roomNumber { get; set; }
+        public string roomType { get; set; }
+        public double pricePerNight { get; set; }
+        public bool isAvailable { get; set; }
+
+        public void displayRoom()
+        {
+            Console.WriteLine($"Room Number: {roomNumber}");
+            Console.WriteLine($"Room Type: {roomType}");
+            Console.WriteLine($"Price Per Night: {pricePerNight}");
+            Console.WriteLine($"Is Available: {isAvailable}");
+        }
+
+
+    }
     internal class Program
     {
         static void Main(string[] args)
